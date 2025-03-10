4708f14f51f3007f728cd8caa83e16cedea959cf
StefanieSteinmair <s.steinmair@students.htl-leonding.ac.at>
2025-03-10 13:03:48 +0100

    überarbeitung

---
+++ HTML/Umsetzung_Lien/Homepage/allgemein.js
@@ -0,0 +1,18 @@
+let isGerman = true;
+
+function toggleLanguage() {
+    const atFlag = document.getElementById('flag-at');
+    const ukFlag = document.getElementById('flag-uk');
+    const label = document.getElementById('language-label');
+
+    if (isGerman) {
+        atFlag.style.opacity = "0"; // Österreich-Flagge ausblenden
+        ukFlag.style.opacity = "1"; // UK-Flagge einblenden
+        label.textContent = "ENG";
+    } else {
+        atFlag.style.opacity = "1"; // Österreich-Flagge einblenden
+        ukFlag.style.opacity = "0"; // UK-Flagge ausblenden
+        label.textContent = "DE";
+    }
+    isGerman = !isGerman;
+}
